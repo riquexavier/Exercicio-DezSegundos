80ade15ebd80b06cfef640857e3992deace1f108
Pedrox | Adm! Dev! <pedrooohhenriquex@hotmail.com>
2024-03-24 13:57:52 -0300

    Resolucao Exercicio Dez Segundos

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine();
+
+Console.WriteLine("Contagem regressiva 10 segundos");
+Console.WriteLine();
+
+Console.WriteLine("Aperte uma tecla para iniciar o programa");
+Console.WriteLine();
+
+Console.ReadKey(false);
+
+Console.Write("0 ");
+Console.Beep(1320, 200);
+Thread.Sleep(1500);
+
+Console.Write("1 ");
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.Write("2 ");
+Console.Beep(1320, 200);
+Thread.Sleep(1000);
+
+Console.Write("3 ");
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.Write("4 ");
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.Write("5 ");
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.Write("6 ");
+Console.Beep(1320, 200);
+Thread.Sleep(1000);
+
+Console.Write("7 ");
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.Write("8 ");
+Console.Beep(1650, 200);
+Thread.Sleep(1000);
+
+Console.Write("9 ");
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.Write("10 ");
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
